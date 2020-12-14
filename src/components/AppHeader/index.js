59b48c58ac6f8b9963629005a2e11a9f2d95bdb4
MArsalanAI3 <marsalanai3@gmail.com>
2020-12-14 21:08:06 +0500

    Initial commit

---
+++ src/components/AppHeader/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+// import deployButton from '../../assets/deploy-to-netlify.svg'
+import logo from '../../assets/logo.svg'
+// import github from '../../assets/github.svg'
+import styles from './AppHeader.css' // eslint-disable-line
+
+const AppHeader = (props) => {
+  return (
+    <header className='app-header'>
+      <div className='app-title-wrapper'>
+        <div className='app-title-wrapper'>
+          <div className='app-left-nav'>
+            <img src={logo} className='app-logo' alt='logo' />
+            <div className='app-title-text'>
+              <h1 className='app-title'>TODO'S Application</h1>
+              <p className='app-intro'>
+              A Serverless JAMStack Todo app with Netlify, Gatsby, GraphQL, and FaunaDB By M.Arsalan
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default AppHeader
